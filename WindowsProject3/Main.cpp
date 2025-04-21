b2064e95810ae469a73df719868d2fa79a484c3b
CalebNoss <163946724+CalebNoss@users.noreply.github.com>
2025-04-21 17:14:37 -0400

    Update Main.cpp
    added auto rendering

--- WindowsProject3/Main.cpp
+++ WindowsProject3/Main.cpp
@@ -9,26 +9,64 @@ using namespace std;
 
 int main()
 {
-
-    Maze tempMaze = Maze();
-    tempMaze.setHeight(200);
-    tempMaze.setWidth(200);
-    tempMaze.setWallCount(1000);
-    tempMaze.generateNewMaze();
-    tempMaze.generateNewWalls();
-    tempMaze.aStarSearchMethod();
-
-    Maze tempMaze1 = Maze();
-    tempMaze1.setHeight(200);
-    tempMaze1.setWidth(200);
-    tempMaze1.setWallCount(0);
-    tempMaze1.generateNewMaze();
-    tempMaze1.generateNewWalls();
-    tempMaze1.breadthFirstSearchMethod();
-
     sf::RenderWindow mainWindow(sf::VideoMode({ 800, 800 }), "Main menu");
     tgui::Gui mainGui(mainWindow);
     mainGui.loadWidgetsFromFile("Maingui.txt", true);
+
+    // make instruction for algorithm button to actually run the algorithms
+    auto renderButton = mainGui.get<tgui::Button>("Render Button");
+
+    renderButton->onPress([&]() {
+        // get heght width & wall count, make error message if wall count is too high
+        float widthVar = mainGui.get<tgui::EditBoxSlider>("Width Slider")->getValue();
+        float heightVar = mainGui.get<tgui::EditBoxSlider>("Height Slider")->getValue();
+        float wallCount = mainGui.get<tgui::EditBoxSlider>("Wall Count Slider")->getValue();
+
+        // if wall count more than number of possible walls
+        int maxWallVar = ((2 * widthVar * heightVar) - widthVar - heightVar);
+        if (wallCount > maxWallVar)
+        {
+            // make error message and quit function
+            sf::RenderWindow errorWindow(sf::VideoMode({ 800, 800 }), "Error");
+            tgui::Gui errorGui(mainWindow);
+            errorGui.loadWidgetsFromFile("errorGui.txt", true);
+            while (errorWindow.isOpen())
+            {
+                while (auto event = errorWindow.pollEvent())
+                {
+                    if (event->is<sf::Event::Closed>())
+                    {
+                        errorWindow.close();
+                        return;
+                    }
+                }
+
+                errorWindow.clear();
+                errorGui.draw();
+                errorWindow.display();
+            }
+        }
+
+        // make maze with the parameters
+        Maze tempMaze = Maze();
+        tempMaze.setWidth(widthVar);
+        tempMaze.setHeight(heightVar);
+        tempMaze.setWallCount(wallCount);
+        tempMaze.generateNewMaze();
+        tempMaze.generateNewWalls();
+
+
+        // for each checkbox, if checked, run function
+        if (mainGui.get<tgui::CheckBox>("Brute Force")->isChecked())
+        {
+            tempMaze.breadthFirstSearchMethod();
+        }
+        if (mainGui.get<tgui::CheckBox>("A* Algo")->isChecked())
+        {
+            tempMaze.aStarSearchMethod();
+        }
+        
+        });
 
     while (mainWindow.isOpen())
     {
@@ -37,7 +75,9 @@ int main()
             mainGui.handleEvent(*event);
 
             if (event->is<sf::Event::Closed>())
+            {
                 mainWindow.close();
+            }
         }
 
         mainWindow.clear();
