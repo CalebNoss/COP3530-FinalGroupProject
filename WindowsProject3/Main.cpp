25cea27b5e4c4cc0bf3b3dd5f74fc17320f73bc9
CalebNoss <163946724+CalebNoss@users.noreply.github.com>
2025-04-21 16:03:13 -0400

    Update Main.cpp
    updated to new function ,still somewhat manual, almost finished

--- WindowsProject3/Main.cpp
+++ WindowsProject3/Main.cpp
@@ -1,123 +1,47 @@
-#define SFML_STATIC
+#include <SFML/Graphics.hpp>
+#include <TGUI/TGUI.hpp>
+#include <TGUI/Backend/SFML-Graphics.hpp>
+
 #include "Maze.h"
-#include "Node.h"
-// #include <SFML/Graphics.hpp>
+
+
+using namespace std;
 
 int main()
 {
-    // sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    // sf::CircleShape shape(100.f);
-    // shape.setFillColor(sf::Color::Green);
-    //
-    // while (window.isOpen())
-    // {
-    //     while (const std::optional event = window.pollEvent())
-    //     {
-    //         if (event->is<sf::Event::Closed>())
-    //             window.close();
-    //     }
-    //
-    //     window.clear();
-    //     window.draw(shape);
-    //     window.display();
-    // }
 
-    //ex path found
-    // Maze m;
-    // m.setWidth(5);
-    // m.setHeight(5);
-    // m.setWallCount(0);
-    //
-    // //Allocate nodes & let generateNewMaze() mark a random entrance/exit
-    // m.generateNewMaze();
-    //
-    // //Assign (x,y) for the Manhattan heuristic
-    // for (int y = 0; y < m.getHeight(); ++y) {
-    //     for (int x = 0; x < m.getWidth(); ++x) {
-    //         m.getNodeAt(y, x)->setPos(x, y);
-    //     }
-    // }
-    //
-    // //Wire up all the four‑way neighbors
-    // m.generateNewWalls();
-    //
-    // //Override to a deterministic start/end
-    // m.setEntranceNode(m.getNodeAt(0, 0));
-    // m.setExitNode(     m.getNodeAt(4, 4));
-    //
-    // cout << "Running A* from (0,0) to (4,4)…\n";
-    // m.aStarSearchMethod();
-    //
-    // return 0;
+    Maze tempMaze = Maze();
+    tempMaze.setHeight(200);
+    tempMaze.setWidth(200);
+    tempMaze.setWallCount(1000);
+    tempMaze.generateNewMaze();
+    tempMaze.generateNewWalls();
+    tempMaze.aStarSearchMethod();
 
-    //ex blocked
-    // Maze m;
-    // m.setWidth(5);
-    // m.setHeight(5);
-    // m.setWallCount(0);
-    // m.generateNewMaze();
-    //
-    //
-    // for (int y = 0; y < m.getHeight(); ++y)
-    //     for (int x = 0; x < m.getWidth(); ++x)
-    //         m.getNodeAt(y, x)->setPos(x, y);
-    //
-    //
-    // m.generateNewWalls();
-    //
-    // //Force start=(0,0) and goal=(4,4)
-    // m.setEntranceNode(m.getNodeAt(0, 0));
-    // m.setExitNode(     m.getNodeAt(4, 4));
-    //
-    // //Carve a full barrier between row 1 and row 2
-    // for (int x = 0; x < m.getWidth(); ++x) {
-    //     m.getNodeAt(1, x)->setSouthNode(nullptr);
-    //     m.getNodeAt(2, x)->setNorthNode(nullptr);
-    // }
-    //
-    // cout << "Running A* on a blocked grid…\n";
-    // m.aStarSearchMethod();
-    //
-    // //Check if exit was ever visited
-    // if (!m.getExitNode()->checkIsVisited()) {
-    //     cout << "A* failed to find a path!" << endl;
-    // }
-    //
-    // return 0;
+    Maze tempMaze1 = Maze();
+    tempMaze1.setHeight(200);
+    tempMaze1.setWidth(200);
+    tempMaze1.setWallCount(0);
+    tempMaze1.generateNewMaze();
+    tempMaze1.generateNewWalls();
+    tempMaze1.breadthFirstSearchMethod();
 
-    const int width  = 20;
-    const int height = 25;
+    sf::RenderWindow mainWindow(sf::VideoMode({ 800, 800 }), "Main menu");
+    tgui::Gui mainGui(mainWindow);
+    mainGui.loadWidgetsFromFile("Maingui.txt", true);
 
-    Maze m;
-    m.setWidth(width);
-    m.setHeight(height);
-    m.setWallCount(0);
+    while (mainWindow.isOpen())
+    {
+        while (auto event = mainWindow.pollEvent())
+        {
+            mainGui.handleEvent(*event);
 
-
-    m.generateNewMaze();
-
-
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
-            m.getNodeAt(y, x)->setPos(x, y);
+            if (event->is<sf::Event::Closed>())
+                mainWindow.close();
         }
+
+        mainWindow.clear();
+        mainGui.draw();
+        mainWindow.display();
     }
-
-
-    m.generateNewWalls();
-
-    m.setEntranceNode(m.getNodeAt(0,    0   ));
-    m.setExitNode(     m.getNodeAt(height-1, width-1));
-
-    // prints “A* path has found the exit!” on success
-    m.aStarSearchMethod();
-
-    if (m.getExitNode()->checkIsVisited()) {
-        cout << "A* completed: path found!" << std::endl;
-    } else {
-        cout << "A* completed: no path found." << std::endl;
-    }
-
-    return 0;
-
 }
